bebe8b200c52f27f35ef85ba6ba7e1bb0d0ace65
Ilya Turkovsky <ilcheg@yandex.ru>
2023-01-10 23:25:06 +0300

    Initial commit

---
+++ src/main/java/ru/netology/BinOps.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class BinOps {
+    public String sum(String a, String b) {
+        int c = Integer.parseInt(a, 2);
+        int d = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(c + d);
+    }
+
+    public String mult(String a, String b) {
+        int c = Integer.parseInt(a, 2);
+        int d = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(c * d);
+    }
+}
---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("111", "1"));
+        System.out.println(bins.mult("10", "11"));
+    }
+}
